ceb66bd2311bced907ca708d6d47c4408afe669a
zhouanan511 <zhouanan511@126.com>
2021-11-17 15:57:59 +0800

    Update GithubTest.java

--- src/main/java/com/GithubTest.java
+++ src/main/java/com/GithubTest.java
@@ -3,5 +3,6 @@ package com;
 public class GithubTest {
     public static void main(String[] args) {
         System.out.println("test-github");
+         System.out.println("test-github  update");
     }
 }
